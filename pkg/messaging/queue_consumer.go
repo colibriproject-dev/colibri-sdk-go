83c48328d09aadac3e87bf9ed70be22e0deb3b6c
Rafael Ramos <arkanjo.ms@gmail.com>
2025-07-26 12:37:43 -0300

    Add comments to QueueConsumer interface to clarify method usage

--- pkg/messaging/queue_consumer.go
+++ pkg/messaging/queue_consumer.go
@@ -2,7 +2,12 @@ package messaging
 
 import "context"
 
+// QueueConsumer defines an interface for consuming messages from a queue and managing the queue's name.
 type QueueConsumer interface {
+
+	// Consume processes a ProviderMessage in a given context and returns an error if the processing fails.
 	Consume(ctx context.Context, providerMessage *ProviderMessage) error
+
+	// QueueName retrieves the name of the queue associated with the consumer.
 	QueueName() string
 }
